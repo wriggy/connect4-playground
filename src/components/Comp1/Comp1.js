c2a4889a694bf0b4c643a4e4ed3224ae2a71ecf7
wriggyj <67104461+wriggy@users.noreply.github.com>
2020-12-27 19:08:40 +0000

    "list or details"

--- src/components/Comp1/Comp1.js
+++ src/components/Comp1/Comp1.js
@@ -3,37 +3,43 @@ import { useState } from 'react'
 
 function Comp1() {
     const data = [
-        { text: "first", id: '001' },
-        { text: "second", id: '002' },
-        { text: "third", id: '003' },
+        { text: "first", cat: "A", id: '001' },
+        { text: "second", cat: "B", id: '002' },
+        { text: "third", cat: "C", id: '003' },
     ]
     const [isClicked, setIsClicked] = useState(false);
     const [selectedItem, setSelectedItem] = useState('001')
 
     const handleClick = e => { setIsClicked(true) }
+    const handleReset = e => { setIsClicked(false)}
+    let ix = data.findIndex(i => i.id === selectedItem);
     let selectedText = data[data.findIndex(i => i.id === selectedItem)].text;
+    let listItems = data.map(item => <SubComp1
+            text={item.text}
+            key={item.id}
+            id={item.id}
+            handleClick={handleClick}
+            setSelectedItem={setSelectedItem}
+            />
+        )
+    let listDetails = (
+        <div>
+            <h3>Selected item = {data[ix].text}</h3>
+            <p>Cat = {data[ix].cat}, Selected id = {selectedItem}</p>
+            <button type="button" 
+                    onClick={handleReset}>
+                   <h3>Back to list</h3>
+            </button>
+        </div>
+    )
 
     return (
         <div>
             <h1>
                 Hello World!
             </h1>
-
-            <h2>Click status = {isClicked ? "true" : "false"}</h2>
-        
-            <h3>Selected item = {selectedText}</h3>
-            
-            
-            <hr />
-            {data.map(item => <SubComp1
-                text={item.text}
-                key={item.id}
-                id={item.id}
-                handleClick={handleClick}
-                setSelectedItem={setSelectedItem}
-            />
-            )}
-
+            {isClicked ? listDetails : listItems}
+            <hr/>
         </div>
     );
 }
